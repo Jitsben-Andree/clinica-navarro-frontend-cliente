4fb63713ca50e65a4f7b33724e3f3280a22a4e4d
jitsbenPC <jitsbenPC@gmail.com>
2026-05-18 00:04:50 -0500

    se creo la estructura de carpetas q seguiremos

---
+++ src/app/shared/components/card-cita/card-cita.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-cita',
+  imports: [],
+  templateUrl: './card-cita.html',
+  styleUrl: './card-cita.css',
+})
+export class CardCita {
+
+}
